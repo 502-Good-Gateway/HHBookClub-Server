1b3fb4c4e56d54fa730f256551daaf4a03c6a279
조현석 <hsjo0227@gmail.com>
2025-08-25 17:02:32 +0900

    initial commit

---
+++ src/main/java/com/goodgateway/hhbookclub/HhBookClubApplication.java
@@ -0,0 +1,13 @@
+package com.goodgateway.hhbookclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhBookClubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HhBookClubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/goodgateway/hhbookclub/HhBookClubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goodgateway.hhbookclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HhBookClubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
